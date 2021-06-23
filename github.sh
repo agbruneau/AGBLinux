bbf0dd0f0a7e84ca22dbc4689fa06c13a89cf157
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2021-06-22 22:51:55 -0400

    Update Github.sh
    Update Github.sh

--- github.sh
+++ github.sh
@@ -10,6 +10,7 @@ echo "Clone des GitHub Repository"
 git clone https://github.com/agbruneau/DevOps.git
 git clone https://github.com/agbruneau/Go.git
 git clone https://github.com/agbruneau/Python.git
+git clone https://github.com/agbruneau/Linux.git
 # git clone https://github.com/agbruneau/Rust.git
 echo "=================="
 echo "Script terminé"

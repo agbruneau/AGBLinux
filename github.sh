2c8bf62785cb91209b4ab7cd66574553b0190557
Andre-Guy Bruneau <agbruneau@gmail.com>
2021-09-09 19:58:56 -0400

    Remove PYthon and DevOps

--- github.sh
+++ github.sh
@@ -7,9 +7,9 @@ mkdir GitHub
 cd GitHub
 echo "=================="
 echo "Clone des GitHub Repository"
-git clone https://github.com/agbruneau/DevOps.git
+# git clone https://github.com/agbruneau/DevOps.git
 git clone https://github.com/agbruneau/Go.git
-git clone https://github.com/agbruneau/Python.git
+# git clone https://github.com/agbruneau/Python.git
 git clone https://github.com/agbruneau/Linux.git
 # git clone https://github.com/agbruneau/Rust.git
 echo "=================="

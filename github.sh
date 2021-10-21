98d2acceea965c5acc3946acdac124390845f3ab
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2021-10-21 17:00:05 -0400

    Remove Git comment symbol

--- github.sh
+++ github.sh
@@ -7,10 +7,10 @@ mkdir GitHub
 cd GitHub
 echo "=================="
 echo "Clone des GitHub Repository"
-# git clone https://github.com/agbruneau/DevOps.git
-# git clone https://github.com/agbruneau/Go.git
-# git clone https://github.com/agbruneau/Python.git
+git clone https://github.com/agbruneau/DevOps.git
+git clone https://github.com/agbruneau/Go.git
+git clone https://github.com/agbruneau/Python.git
 git clone https://github.com/agbruneau/Linux.git
-# git clone https://github.com/agbruneau/Rust.git
+git clone https://github.com/agbruneau/Rust.git
 echo "=================="
 echo "Script terminé"

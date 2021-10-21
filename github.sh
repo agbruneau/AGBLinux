358a8a5fe9856c00ad2759d4d71cacc8a49b7f4f
André-Guy Bruneau <45982482+agbruneau@users.noreply.github.com>
2021-10-21 16:54:14 -0400

    Remove git go repository

--- github.sh
+++ github.sh
@@ -8,7 +8,7 @@ cd GitHub
 echo "=================="
 echo "Clone des GitHub Repository"
 # git clone https://github.com/agbruneau/DevOps.git
-git clone https://github.com/agbruneau/Go.git
+# git clone https://github.com/agbruneau/Go.git
 # git clone https://github.com/agbruneau/Python.git
 git clone https://github.com/agbruneau/Linux.git
 # git clone https://github.com/agbruneau/Rust.git
